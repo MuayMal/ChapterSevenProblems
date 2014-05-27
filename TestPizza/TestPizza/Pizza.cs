19f1cee363c2286ee3d958bdbf3b961bedb9cb90
Malachi' <mtoomer81@gmail.com>
2014-05-27 10:12:04 -0400

    hmfn

---
+++ TestPizza/TestPizza/Pizza.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Pizza
+    {
+        private double diameter;
+        private double price;
+        private string topping;
+
+
+        public Pizza(double diameter)
+        {
+            this.diameter = diameter;
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+
+            set
+            {
+                diameter = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return topping;
+            }
+            set
+            {
+                topping = value;
+            }
+             
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price = 13.99;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+    }
+}
---
+++ TestPizza/TestPizza/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+            Pizza pizza = new Pizza(12);
+            pizza.Name = "Pepperoni";
+
+            pizza.Diameter = double d;
+            pizza.Price = dou
+
+
+            Console.WriteLine(pizza);
+        }
+    }
+}
